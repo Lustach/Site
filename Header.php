bee9a629a73b0ef17d7cf53d2a54dea2748cec03
Lustach <lusta.vlad2001@gmail.com>
2019-03-31 12:27:41 +0300

    POMOGITE

--- Header.php
+++ Header.php
@@ -9,15 +9,23 @@
     <link rel="stylesheet" href="index.css">
     <title>Lustacho_Shop</title>
     <div class="location">
-    <a href="Index.php">Москва</a>
-    <a href="Index.php">Магазины</a>
-    <a href="Index.php">Покупателям</a>
-    <a href="Index.php">Войти</a>
-    <a href="Index.php">Регистрация</a>
+        <a href="Index.php">Москва</a>
+        <a href="Index.php">Магазины</a>
+        <a href="Index.php">Покупателям</a>
+    <!-- <div class="signin"><a href="Index.php">Войти</a></div>
+    <div class="registr"><a href="Index.php">Регистрация</a></div> -->
+        <div class="phonenumber">Наш телефон: 099-091-02-60</div>
+        <div class=signin>
+            <a href="Index.php">Войти</a>
+            <a href="Index.php">Регистрация</a>
+        </div>
     </div>
     <div class="header">
-    <a href="Index.php">Корзина</a>
+        <div class="basket"><a href="Index.php">Корзина 1 товар на сумму 20000р </a></div>
+        <div class="comparison"><a href="Index.php">Сравнение</a></div>
     </div>
+    <div class="img1"><a href="Index.php"><img src="Images/DNs.jpg" class="img" alt="IMG"></a></div>
+    
     <!-- <div class="location"><a href="Index.php">Магазины</a></div>
     <div class="location"><a href="Index.php">Покупателям</a></div>
     <span>+380990910260( круглосуточно)</span>
--- Index.php
+++ Index.php
@@ -1,6 +1,29 @@
 <? require('Header.php')?>
     <body>
-
+    <div class="navigation">
+        <div class="navblock"><a href="">Ноутбуки</a></div>
+        <div class="navblock"><a href="">Комплектующие для ПК</a></div>
+        <div class="navblock"><a href="">Компьютеры и периферия</a></div>
+        <div class="navblock"><a href="">Смартфоны</a></div>
+        <div class="navblock"><a href="">Телевизоры</a></div>
+        <div class="navblock"><a href="">Бытовая техника</a></div>
+        <div class="navblock"><a href="">Аудиотехника</a></div>
+        <div class="navblock"><a href="">Инструменты</a></div>
+        <div class="navblock"><a href="">Услуги</a></div>
+    </div>
+    <!-- <div class="News">
+        <h3>Здесь будут распологаться важные новости </h3>
+    </div> -->
+    <div class="actionbanner">
+        <!-- тут должна быть смеянемость картинок -->
+        <img class="imgnew" src="Images/Kek.jpg" alt="Картинка">
+    </div>
+    <div class="spanl">
+        <h1>Лучшие предложения</h1>
+    </div>
+    <div class="popular">
+        Ghtljk;tDSFADF
+    </div>
     </body>
 <? require('Footer.php')?>
 
